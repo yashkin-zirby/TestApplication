6f32168798d0f53d42ceb576829a2c7608b84c89
Yashkin <petrovskistas01@gmail.com>
2024-01-21 20:53:16 +0300

    Add class for read accounting data from Excel

---
+++ Pages/WorkWithExcelPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestApplication.Utils;
+
+namespace TestApplication.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkWithExcelPage.xaml
+    /// </summary>
+    public partial class WorkWithExcelPage : Page
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        public WorkWithExcelPage()
+        {
+            InitializeComponent();
+            openFileDialog.Filter = "Excel File|*.xlsx;*.xls";
+            openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            openFileDialog.Title = "Choose file for import to database";
+            openFileDialog.DefaultExt = ".xlsx";
+        }
+
+        private void ExportExcelDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = openFileDialog.ShowDialog();
+            if(result.HasValue && result.Value)
+            {
+                try
+                {
+                    ExcelAccountingReader reader = new ExcelAccountingReader(openFileDialog.FileName);
+                    var dt = reader.ReadDataRowsInRange(5, 12);
+                    var rows = dt.Rows;
+                    MessageBox.Show(dt.Rows.Count.ToString());
+                    for(int i = 0; i < rows.Count; i++)
+                    {
+                        MessageBox.Show($"{rows[i][0]}|{rows[i][1]}|{rows[i][2]}|{rows[i][3]}|{rows[i][4]}");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Utils/ExcelAccountingReader.cs
@@ -0,0 +1,68 @@
+﻿using Syncfusion.XlsIO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Syncfusion.XlsIO.Implementation;
+using System.Windows;
+
+namespace TestApplication.Utils
+{
+    class ExcelAccountingReader : IDisposable
+    {
+        private static ExcelEngine? engine = null;
+        private static IApplication? application = null;
+        private IWorkbook workbook;
+        public ExcelAccountingReader(string fileName)
+        {
+            if(engine == null)
+            {
+                engine = new ExcelEngine();
+            }
+            if(application == null)
+            {
+                application = engine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+            }
+
+            FileStream inputStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            workbook = application.Workbooks.Open(inputStream);
+            workbook.Worksheets[0].ExportDataTableEvent += new WorksheetImpl.ExportDataTableEventHandler((args) =>
+            {
+                string cell = $"{args.ExcelValue}";
+                decimal value;
+                if (!decimal.TryParse(cell, out value) || args.ExcelColumnIndex == 1 && value < 1000)
+                {
+                    args.ExportDataTableAction = ExportDataTableActions.SkipRow;
+                    return;
+                }
+                args.DataTableValue = value;
+            });
+        }
+        public int LastRow { get
+            {
+                return workbook.Worksheets[0].UsedRange.End.Row;
+            } 
+        }
+        public DataTable ReadDataRowsInRange(int startRow, int length)
+        {
+            return workbook.Worksheets[0].ExportDataTable(startRow, 1, length, 5,
+                ExcelExportDataTableOptions.None);
+        }
+        public string ReadString(string cell)
+        {
+            return workbook.Worksheets[0].Range[cell].Value;
+        }
+        public double? ReadNumber(string cell)
+        {
+            return workbook.Worksheets[0].Range[cell].HasNumber? workbook.Worksheets[0].Range[cell].Number : null;
+        }
+
+        public void Dispose()
+        {
+            workbook.Close();
+        }
+    }
+}
